96ff1df43d614957ac25795843a3c657c5c2d4f5
Mehmet Can Ertuzun <mcertuzun@gmail.com>
2020-09-02 23:43:46 +0300

    Init

---
+++ Assets/Scripts/Helper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+public class Helper
+{
+    public static List<T> SameTypeOfComponents<T>(MonoBehaviour thisObject) where T : class
+    {
+        BindingFlags bindingFlags = (BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly);
+        FieldInfo[] fields = thisObject.GetType().GetFields(bindingFlags);
+        List<T> tempList = new List<T>();
+        foreach (FieldInfo field in fields)
+        {
+            T value = field.GetValue(thisObject) as T;
+
+            if (value != null)
+            {
+               tempList.Add(value);
+            }
+        }
+        return tempList;
+    }
+
+  
+   
+}
---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace Managers
+{
+    public class GameManager : MonoBehaviour
+    {
+        //Game events for UI section
+        public static event Action CanvasManager;
+        // Start is called before the first frame update
+        void Start()
+        {
+            CanvasManager?.Invoke();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}

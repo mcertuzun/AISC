afc776cad4bb3a5f3c0dbd637d3d9ad9c33f3f1e
mcertuzun <mcertuzun@gmail.com>
2020-11-21 01:46:44 +0300

    Working on AI state machine and EventSystem with ScriptableObject

---
+++ Assets/Scripts/StateMachineAI/States/State.cs
@@ -0,0 +1,42 @@
+﻿using StateMachineAI.Actions;
+using UnityEngine;
+
+namespace StateMachineAI.States
+{
+    [CreateAssetMenu(fileName = "FILENAME", menuName = "MENUNAME", order = 0)]
+    public class State : ScriptableObject
+    {
+        public Action[] actions;
+        public Transition[] transitions;
+        public Color sceneGizmoColor = Color.grey;
+
+        public void UpdateState(StateController controller)
+        {
+            DoActions(controller);
+            CheckTransitions(controller);
+        }
+        
+        private void DoActions(StateController controller)
+        {
+            for (int i = 0; i < actions.Length; i++) {
+                actions [i].Act (controller);
+            }
+        }
+
+        private void CheckTransitions(StateController controller)
+        {
+            for (var i = transitions.Length - 1; i >= 0; i--) 
+            {
+                var decisionSucceeded = transitions [i].decision.Decide (controller);
+
+                if (decisionSucceeded) 
+                    controller.TransitionToState (transitions [i].trueState);
+                else
+                    controller.TransitionToState (transitions [i].falseState);
+                
+            }
+        }
+
+
+    }
+}
